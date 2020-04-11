fd0a4a2a5fc5f18d727b1dd91905f6ae8b962358
Simon <simonfong6@gmail.com>
2020-04-10 23:43:37 -0700

    App to show proxiying

--- rails-react-demo/client/src/App.js
+++ rails-react-demo/client/src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const fetchTestDatas = function () {
+  fetch('/api/v1/tests')
+    .then(res => res.json())
+    .then((response) => { console.log("Test datas response", response); })
+    .catch((error) => { console.log("Error while fetching test datas", error); })
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fetchTestDatas}>
+        Fetch Test Datas
+      </button>
     </div>
   );
 }
